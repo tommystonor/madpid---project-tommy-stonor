1c45568c9bcc777b865c6be520b8da12e86d40f0
tommystonor <77493413+tommystonor@users.noreply.github.com>
2021-02-05 12:46:22 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,20 @@
-scene.setBackgroundColor(8)
-let MySpaceplane = sprites.create(img`
+let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . f f 2 . . . . . . . . 
-    . . . . . f 2 2 2 . . . . . . . 
-    . . . . . f 2 2 f 2 . . . . . . 
-    . . . . . f 2 2 9 f 2 . . . . . 
-    . . . . . f 2 2 9 9 f 2 2 . . . 
-    . . . . . f 2 2 9 f 2 . . . . . 
-    . . . . . f 2 2 f 2 . . . . . . 
-    . . . . . f 2 2 2 . . . . . . . 
-    . . . . . f f 2 . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 . . . . 2 . . . . . 
+    . . . . . 2 . 2 2 . 2 . . . . . 
+    . . . . . 2 . . . . 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . 2 2 2 2 . . . . . . 
+    . . . . . 2 2 . . 2 2 . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-MySpaceplane.setFlag(SpriteFlag.StayInScreen, true)
-info.setLife(3)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`level1`)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,5 +3,37 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010000000000000000000101010101010100000000000000000001010101010101000001010101010000010101010101010000010101010100000101010101010100000101010101000001010101010101000001000000000000010101010101010000010000000000000001010101010100000101010101010000010101010101000000000000000100000101010101010000000000000001000001010000020100000101010000010000010100000201000001010100000100000101000001010000010101000001000000000000010100000101010000010000000000000101020201010101010101010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.saplingOak,sprites.dungeon.chestClosed], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
